fcb7f872e4fad3305e52209ed189192afdf8d478
abdurahmon2909 <abdurahmon29091996@gmail.com>
2026-04-05 18:24:06 +0500

    Fix imports: use default imports for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-// React importini o'chirish (React 17+ da kerak emas)
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { CatalogPage } from './pages/CatalogPage';
-import { CartPage } from './pages/CartPage';
-import { CheckoutPage } from './pages/CheckoutPage';
-import { SuccessPage } from './pages/SuccessPage';
+﻿import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import CatalogPage from './pages/CatalogPage';
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+import SuccessPage from './pages/SuccessPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path='/' element={<Layout />}>
           <Route index element={<CatalogPage />} />
-          <Route path="cart" element={<CartPage />} />
-          <Route path="checkout" element={<CheckoutPage />} />
-          <Route path="success" element={<SuccessPage />} />
+          <Route path='cart' element={<CartPage />} />
+          <Route path='checkout' element={<CheckoutPage />} />
+          <Route path='success' element={<SuccessPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
